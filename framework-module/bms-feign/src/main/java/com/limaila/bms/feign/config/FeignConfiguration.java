864ef157267c937d23eaf5f08ac79e6781ea6ce8
husenhaung <husenhuang@can-dao.com>
2020-09-01 20:50:16 +0800

    提交测试代码

---
+++ framework-module/bms-feign/src/main/java/com/limaila/bms/feign/config/FeignConfiguration.java
@@ -0,0 +1,40 @@
+package com.limaila.bms.feign.config;
+
+
+import feign.Feign;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
+import org.springframework.boot.autoconfigure.web.servlet.WebMvcRegistrations;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.annotation.AnnotatedElementUtils;
+import org.springframework.web.servlet.mvc.method.annotation.RequestMappingHandlerMapping;
+
+/***
+ 说明:
+ @author MrHuang
+ @date 2020/3/23 20:33
+ @desc
+ ***/
+@Configuration
+@ConditionalOnClass({Feign.class})
+public class FeignConfiguration {
+
+    @Bean
+    public WebMvcRegistrations feignWebRegistrations() {
+        return new WebMvcRegistrations() {
+            @Override
+            public RequestMappingHandlerMapping getRequestMappingHandlerMapping() {
+                return new FeignRequestMappingHandlerMapping();
+            }
+        };
+    }
+
+    private static class FeignRequestMappingHandlerMapping extends RequestMappingHandlerMapping {
+        @Override
+        protected boolean isHandler(Class<?> beanType) {
+            return super.isHandler(beanType) &&
+                    !AnnotatedElementUtils.hasAnnotation(beanType, FeignClient.class);
+        }
+    }
+}
--- framework-module/bms-feign/src/main/java/com/limaila/bms/feign/interceptor/ProviderHandlerInterceptor.java
+++ framework-module/bms-feign/src/main/java/com/limaila/bms/feign/interceptor/ProviderHandlerInterceptor.java
@@ -1,4 +1,4 @@
-package com.limaila.bms.framework.feign;
+package com.limaila.bms.feign.interceptor;
 
 import com.alibaba.fastjson.JSON;
 import com.limaila.bms.common.constants.HeaderConstant;
