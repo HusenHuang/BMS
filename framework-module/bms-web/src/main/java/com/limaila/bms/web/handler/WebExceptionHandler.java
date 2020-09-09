f20d61026240ad33c8d7476e75be2d5b43bc311e
husenhaung <husenhuang@can-dao.com>
2020-09-09 21:56:16 +0800

    feat/   1.增加Web模块统一异常处理

---
+++ framework-module/bms-web/src/main/java/com/limaila/bms/web/handler/WebExceptionHandler.java
@@ -0,0 +1,28 @@
+package com.limaila.bms.web.handler;
+
+import com.limaila.bms.common.response.RestRSP;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/***
+ 说明: 
+ @author MrHuang
+ @date 2020/9/9 21:53
+ @desc
+ ***/
+@Slf4j
+@ControllerAdvice
+public class WebExceptionHandler {
+
+    @ResponseBody
+    @ExceptionHandler(value = Exception.class)
+    public RestRSP defaultErrorHandler(HttpServletRequest request, HttpServletResponse response, Exception e) throws Exception {
+        log.error("WebExceptionHandler ", e);
+        return RestRSP.systemFailed("系统异常");
+    }
+}
