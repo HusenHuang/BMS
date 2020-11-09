04cf7cd80a5d656607026b8a4b1022a780bdb37e
husenhaung <husenhuang@can-dao.com>
2020-11-09 16:03:36 +0800

    修改simple-deplpy.sh脚本

--- simple-deploy.sh
+++ simple-deploy.sh
@@ -1,5 +1,5 @@
 # 1.停止当前实例
-cd /worker/git-worker/BMS
+cd /data/config/bms/BMS
 kubectl delete -f service-module/bms-tool-service/kubernetes/
 kubectl delete -f service-module/bms-authority-service/kubernetes/
 kubectl delete -f gateway-module/bms-gateway/kubernetes/
@@ -8,17 +8,17 @@ kubectl delete -f gateway-module/bms-gateway/kubernetes/
 mvn clean install -Dmaven.test.skip=true
 
 # 3.构建docker镜像
-cd /worker/git-worker/BMS/service-module/bms-tool-service
+cd /data/config/bms/BMS/service-module/bms-tool-service
 docker build -f docker/Dockerfile -t bms-tool-service:1.0 .
 
-cd /worker/git-worker/BMS/service-module/bms-authority-service
+cd /data/config/bms/BMS/service-module/bms-authority-service
 docker build -f docker/Dockerfile -t bms-authority-service:1.0 .
 
-cd /worker/git-worker/BMS/gateway-module/bms-gateway
+cd /data/config/bms/BMS/gateway-module/bms-gateway
 docker build -f docker/Dockerfile -t bms-gateway:1.0 .
 
 # 4. kubernetes部署
-cd /worker/git-worker/BMS
+cd /data/config/bms/BMS
 kubectl apply -f service-module/bms-tool-service/kubernetes/
 kubectl apply -f service-module/bms-authority-service/kubernetes/
 kubectl apply -f gateway-module/bms-gateway/kubernetes/
